8f5e9d151a7e5ae18d892655248ee924bdb68345
Juyeong Maing <mjy9088@naver.com>
2025-05-27 23:59:30 +0900

    remove unused

--- app/Game.tsx
+++ app/Game.tsx
@@ -1,7 +1,5 @@
 import { Canvas } from "@react-three/fiber";
 import { ComponentType, useMemo, useState } from "react";
-import styles from "./Game.module.css";
-import { contrastColor } from "./lib/color/contrastColor";
 import { GameState, GameStateWithoutId } from "./lib/GameState";
 import { simpleHashColor } from "./lib/simpleHashColor";
 import { SceneComponentProps } from "./SceneComponentProps";
@@ -10,34 +8,6 @@ import { sceneRouter } from "./scenes/sceneRouter";
 export interface GameProps {
   state: GameState;
   setState: (newState: GameStateWithoutId) => void;
-}
-
-interface SplashProps {
-  id: string;
-  color: string;
-}
-
-function Splash({ id, color }: SplashProps) {
-  return (
-    <div className={styles["container"]}>
-      <div className={styles["content"]} style={{ backgroundColor: color }}>
-        <div className={styles["titleContainer"]}>
-          <div
-            className={styles["title"]}
-            style={{ color: contrastColor(color) }}
-          >
-            {id}
-          </div>
-        </div>
-
-        <div className={styles["navbar"]}>
-          <div className={styles["navButton"]} />
-          <div className={styles["navButtonWide"]} />
-          <div className={styles["navButton"]} />
-        </div>
-      </div>
-    </div>
-  );
 }
 
 export default function Game({ state, setState }: GameProps) {
